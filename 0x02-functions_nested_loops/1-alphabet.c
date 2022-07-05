fa6b0ce5009beca1c2f86ecb7afb6ef163419b6b
CynthiaMkabane <cmkabane@gmail.com>
2022-07-05 13:07:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Begin Here
+ *print a-z and A-Z using putchar
+ *Return: Value 0 (true)
+ **/
+
+void print_alphabet(void)
+
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+
+	}
+	_putchar('\n');
+
+}
